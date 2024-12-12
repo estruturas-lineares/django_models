0791b598011242356757057897be5bc1c22ea5dd
Karen Dantas <karennobre74@gmail.com>
2024-12-12 17:27:52 -0300

    finish task2 again

--- core/models.py
+++ core/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField('name',max_length=200)
+    creation_date = models.DateTimeField('date',auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class Supplier(models.Model):
+    name = models.CharField('name',max_length=200)
+    cnpj =  models.CharField('cnpj',max_length=14, unique=True)
+    creation_date = models.DateTimeField('date',auto_now_add=True)
+    def __str__(self):
+        return self.name
 # Create your models here.
 class Product(models.Model):
     name = models.CharField('name',max_length=200)
@@ -7,3 +19,8 @@ class Product(models.Model):
     description = models.TextField('description',blank=True)
     price = models.DecimalField('price',max_digits=10, decimal_places = 2)
     creation_date = models.DateTimeField('date',auto_now_add=True)
+    categories = models.ManyToManyField(Category)
+    supplier = models.ForeignKey(Supplier, on_delete= models.CASCADE)
+    
+    def __str__(self):
+        return self.name
