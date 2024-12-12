4d47d3a7a1cef5503ada8678d2d8569da5240d41
Karen Dantas <karennobre74@gmail.com>
2024-12-12 16:16:26 -0300

    feat: create pages

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index )
+    path('', views.index ),
+    path('products/', views.products, name= 'products'),
+    path('categoryes/', views.categorys, name= 'categoryes'),
+    path('supplier/', views.supplier, name= 'supplier' ),
+    path('<int:product_id>', views.details)
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from .models import Product, Category,Supplier
 
 # Create your views here.
 def index (request):
     return render(request,"core/index.html")
+
+def products(request):
+    products_model = Product.objects.all()
+    context = {'products': products_model}
+    return render(request, "core/products.html", context)
+
+def categorys(request):
+    categorys_model = Category.objects.all()
+    context = {'categoryes': categorys_model}
+    return render(request, "core/categoryes.html", context)
+
+def supplier(request):
+    suppliers = Supplier.objects.all()
+    context = {'suppliers': suppliers}
+    return render(request, "core/supplier.html", context)
+
+def details(request, product_id):
+    prodcut = Product.objects.get(id = product_id)  
+    context = {'product': prodcut}
+    return render(request, "core/details.html", context)
