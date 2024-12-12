d449ccbdb9d3760602339ddf0633a60e257ce83c
Karen Dantas <karennobre74@gmail.com>
2024-12-12 16:21:37 -0300

    feat: create Categoryes and Supplier models, and add relations to Product"

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Category, Supplier
+from .models import Product
 # Register your models here.
 
 
@@ -10,5 +10,3 @@ class ProductAdmin (admin.ModelAdmin):
     ordering = ['-creation_date',]
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(Category)
-admin.site.register(Supplier)
